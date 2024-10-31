17f4b423464a939f1ed4a36b4ef99f8410963b1a
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-10-31 16:10:25 +0200

    Updating some components by adding the generateStsticParams for ststic site generation and creating some dynamic pages

--- src/app/(page-routes)/blog/page.tsx
+++ src/app/(page-routes)/blog/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <>
+      <section className="md:px-8 px-4 py-14">
+        <h1 className="font-semibold  md:text-5xl text-3xl pb-5 md:pb-12  mt-28 text-center">
+          Blog Coming Soon!!!
+        </h1>
+      </section>
+    </>
+  );
 };
 
 export default Blog;
