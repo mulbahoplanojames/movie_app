c31e05c5286350abb9f13f62c2e74275df958aa9
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2026-09-01 07:48:26 +0200

    chore: clean the repo

--- src/products/products.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductsController } from './products.controller.js';
-
-@Module({
-  controllers: [ProductsController],
-})
-export class ProductsModule {}
--- src/users/users.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller.js';
-
-@Module({
-  controllers: [UsersController],
-})
-export class UsersModule {}
